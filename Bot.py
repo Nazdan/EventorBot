bba04aaede21400f300109b4d2b0ef1bd18e73b0
Nazdan <nazarovd00@gmail.com>
2019-08-10 12:53:11 +0300

    полу бот

--- Bot.py
+++ Bot.py
@@ -1,11 +1,15 @@
 import telebot
 import time
+import pymysql
 from telebot import types
 import add
 
 token = "721442351:AAHTxBWlgwvtz3tmDhPwcRkKeXo5WxC6lgk"
 count = 0
 
+
+db = pymysql.connect(host='127.0.0.1', user='root', passwd='', db='events', charset='utf8mb4')
+cursor = db.cursor()
 
 hide = types.ReplyKeyboardRemove()  # Спрятанная клавиатура
 choice1 = types.ReplyKeyboardMarkup()
@@ -36,17 +40,17 @@ def choose(message):
         bot.register_next_step_handler(message, look_)
     else:
         bot.send_message(message.chat.id, 'Выберите тему', reply_markup=theme_markup)
-        bot.register_next_step_handler(message, add.theme(message, count))
+        bot.register_next_step_handler(message, add.theme)
 
 
 def look_(message):  # Просмотр ивентов
     count = 1
-    list = []
+    sndm = []
     with open('data.txt') as file:
         for i in file:
-            list.append(i)
-        print(list)
-        for i in list:
+            sndm.append(i)
+        print(sndm)
+        for i in sndm:
             bot.send_message(message.chat.id, str(count) + ') ' + i + '\n')
             count += 1
     start(message, False)
