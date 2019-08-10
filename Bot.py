fe76f6917f8e3f9dc797581b50d44a018c2a5745
Nazdan <nazarovd00@gmail.com>
2019-08-10 18:54:45 +0300

    почти бот с сортиров очкой и отменой, а ещё городами

--- Bot.py
+++ Bot.py
@@ -5,14 +5,19 @@ from telebot import types
 token = '721442351:AAHTxBWlgwvtz3tmDhPwcRkKeXo5WxC6lgk'
 count = 10
 the = ''
+my_town = ''
+what = True
 
 db = pymysql.connect(host='127.0.0.1', user='root', passwd='', db='events', charset='utf8mb4')
 cursor = db.cursor()
 
 hide = types.ReplyKeyboardRemove()  # Спрятанная клавиатура
-choice1 = types.ReplyKeyboardMarkup()
 
+choice1 = types.ReplyKeyboardMarkup()
 choice1.row(types.KeyboardButton('Посмотреть'), types.KeyboardButton('Добавить'))
+
+town_markup = types.ReplyKeyboardMarkup()
+town_markup.row(types.KeyboardButton('Москва'), types.KeyboardButton('Казань'))
 
 meet = types.KeyboardButton('Встреча')
 sport = types.KeyboardButton('Спорт')
@@ -41,7 +46,21 @@ def start(message, ter=True):
 
 
 def choose(message):
+    global what
     if message.text == 'Посмотреть':  # Просмотр ивентов
+        what = True
+        bot.send_message(message.chat.id, 'Выберите город', reply_markup=town_markup)
+        bot.register_next_step_handler(message, town_)
+    else:
+        what = False
+        bot.send_message(message.chat.id, 'Выберите город', reply_markup=town_markup)
+        bot.register_next_step_handler(message, town_)
+
+
+def town_(message):
+    global my_town
+    my_town = message.text
+    if what:
         bot.send_message(message.chat.id, 'Выберите тему', reply_markup=theme_markup)
         bot.register_next_step_handler(message, look_)
     else:
@@ -53,9 +72,9 @@ def look_(message):  # Просмотр ивентов
     snd = ''
     theme4sort = message.text
     if theme4sort == 'Всё':
-        command = 'SELECT * FROM `all` WHERE 1'
+        command = "SELECT * FROM `all` WHERE town = '{}'".format(my_town)
     else:
-        command = "SELECT * FROM `all` WHERE theme = '{}'".format(theme4sort)
+        command = "SELECT * FROM `all` WHERE theme = '{}' AND town = '{}'".format(theme4sort, my_town)
     cursor.execute(command)
     data = cursor.fetchall()
     for event in data:
@@ -84,9 +103,9 @@ def create(message):
         global count
         l = message.text.split('\n')
         print(l)
-        command = "INSERT INTO `all`(`user_id`, `event_id`, `name`, `date`, `time`, `adress`, `theme`, `description`)" \
-                  " VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(message.from_user.id, str(count), l[0],
-                                                                                   l[1], l[2], l[3], the, l[4])
+        command = "INSERT INTO `all`(`user_id`, `event_id`, `name`, `date`, `time`, `adress`, `theme`, `town`, `description`)" \
+                  " VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(message.from_user.id, str(count), l[0],
+                                                                                   l[1], l[2], l[3], the, my_town, l[4])
         cursor.execute(command)
         db.commit()
         count += 1
