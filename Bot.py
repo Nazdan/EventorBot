163fe33344696bc5cf938638ad0e833ca7f852a7
Kamil <ninevskykn@gmail.com>
2019-08-10 14:56:44 +0300

    stupid push

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ import telebot
 import pymysql
 from telebot import types
 
-token = "721442351:AAHTxBWlgwvtz3tmDhPwcRkKeXo5WxC6lgk"
+token = '721442351:AAHTxBWlgwvtz3tmDhPwcRkKeXo5WxC6lgk'
 count = 10
 the = ''
 
