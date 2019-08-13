2bdc155736a39006fc6d1fc542458fd2f550b066
Nazdan <nazarovd00@gmail.com>
2019-08-13 14:11:31 +0300

    почти бот с сортиров очкой и отменой, а ещё городами и удаленкой + DialogFlow - описание

--- Bot.py
+++ Bot.py
@@ -1,11 +1,17 @@
 import telebot
 import pymysql
 from telebot import types
+from time import timezone
+import apiai
+import json
+import datetime
+
 
 token = '721442351:AAHTxBWlgwvtz3tmDhPwcRkKeXo5WxC6lgk'
 count = 10
 the = ''
 my_town = ''
+my_address = ''
 what = True
 
 db = pymysql.connect(host='sql7.freemysqlhosting.net', user='sql7301609', passwd='8swRXjLsGN', db='sql7301609', charset='utf8mb4')
@@ -21,16 +27,17 @@ town_markup.row(types.KeyboardButton('Москва'), types.KeyboardButton('Ка
 
 meet = types.KeyboardButton('Встреча')
 sport = types.KeyboardButton('Спорт')
+music = types.KeyboardButton('Музыка')
 other = types.KeyboardButton('Другое')
 abort = types.KeyboardButton('Отмена')
 
 theme_markup = types.ReplyKeyboardMarkup()
 theme_markup_add = types.ReplyKeyboardMarkup()
 
-theme_markup.row(meet, sport)
+theme_markup.row(meet, sport, music)
 theme_markup.row(other, types.KeyboardButton('Всё'))
 
-theme_markup_add.row(meet, sport)
+theme_markup_add.row(meet, sport, music)
 theme_markup_add.row(other, abort)
 
 bot = telebot.TeleBot(token=token)
@@ -78,7 +85,7 @@ def look_(message):  # Просмотр ивентов
     cursor.execute(command)
     data = cursor.fetchall()
     for event in data:
-        snd += event[6] + " " + event[3] + '\n' + event[7] + '\n' + event[1] + '\n' * 2
+        snd += event[2] + " " + str(event[3]) + '\n' + event[5] + '\n' + event[7] + '\n' * 2
     bot.send_message(message.chat.id, snd)
     start(message, False)
 
@@ -90,9 +97,7 @@ def theme(message):
         global the
         the = message.text
         bot.send_message(message.chat.id,
-                         "Напишите информацию о  вашем событии по шаблону \n Название \n Дата \n Время "
-                         "\n Место проведения \n Описание \n Вместо пустых полей не забывайте ставить "
-                         "прочерки(-)")
+                         "Напишите адресс места проведения мероприятия")
         bot.register_next_step_handler(message, create)
 
 
@@ -100,17 +105,60 @@ def create(message):
     if message.text == 'Отмена':
         start(message, False)
     else:
-        global count
-        l = message.text.split('\n')
-        print(l)
-        command = "INSERT INTO `events_event`(`event_text`, `event_owner`, `event_date`, `event_theme`, " \
-                  "`event_city`, `event_name`, `event_address`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', " \
-                  "'{}')".format(l[4], message.from_user.id, l[1] + ' ' + l[2], the, my_town, l[0], l[3])
-        cursor.execute(command)
-        db.commit()
-        count += 1
-        bot.send_message(message.chat.id, 'ГОТОВО!')
+        global my_address
+        my_address = message.text
+        bot.send_message(message.chat.id,
+                         "Напишите информацию о  вашем событии")
+        bot.register_next_step_handler(message, crutch)
+
+
+def crutch(message):
+    if message.text == 'Отмена':
         start(message, False)
+    else:
+        dialogflow(message, [])
+
+
+def dialogflow(message, context):
+    request = apiai.ApiAI('716c31c2ff3a4fd390b14923be5b68e5').text_request()
+    request.lang = 'ru'
+    request.session_id = str(message.chat.id)
+    request.query = message.text
+    request.contexts = context
+    response = json.loads(request.getresponse().read().decode('utf-8'))
+    print(response)
+    try:
+        if response['result']['resolvedQuery'] == 'Отмена':
+            print(1)
+            bot.send_message(message.chat.id, "Что выберешь?", reply_markup=choice1)
+            bot.register_next_step_handler(message, choose)
+        elif response['result']['contexts'][0]['parameters']['theme'] == '' or \
+                response['result']['contexts'][0]['parameters']['date'] == '' or response['result']['contexts'][0]['parameters']['time'] == '':
+            bot.send_message(message.chat.id, response['result']['fulfillment']['speech'])
+            bot.register_next_step_handler(message, dialogflow, response['result']['contexts'])
+            print(2)
+        elif response['result']['metadata']['intentName'] == 'create_event':
+            print(3)
+            bot.send_message(message.chat.id, response['result']['fulfillment']['speech'])
+            bot.register_next_step_handler(message, dialogflow, response['result']['contexts'])
+        elif response['result']['metadata']['intentName'] == 'yes_event':
+            print('oooo')
+            # типо записали в БД эти два значения
+            bot.send_message(message.chat.id, response['result']['fulfillment']['speech'])
+            name = response['result']['contexts'][0]['parameters']['theme']
+            date = response['result']['contexts'][0]['parameters']['date']
+            time = response['result']['contexts'][0]['parameters']['time']
+            print(date, time)
+            command = "INSERT INTO `events_event`(`event_owner`, `event_name`, `event_date`, `event_theme`, " \
+                      "`event_address`, `event_city`, `event_text`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', " \
+                      "'Без описания')".format(message.from_user.id, name, date +  " " + time, the, my_address, my_town)
+            cursor.execute(command)
+            db.commit()
+            bot.send_message(message.chat.id, 'Вы успешно создали мероприятие! Выбирайте что дальше...', reply_markup=choice1)
+            bot.register_next_step_handler(message, choose)
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, response['result']['fulfillment']['speech'])
 
 
 bot.polling(none_stop=True)
